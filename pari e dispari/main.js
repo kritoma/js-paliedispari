e67927145a27333c3e3481ab16ceab917c4f3f34
kritoma <toma.kristian97@gmail.com>
2022-05-06 17:51:04 +0200

    pari o dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -15,22 +15,33 @@ function isOdd(n) {
 
 // main  
 // scelta pari o dispari
+// let userChoice = prompt("pari o dispari");
 let userChoice;
 do {
     userChoice = prompt("pari o dispari");
-} while ( (userChoice === "pari") || (userChoice === "dispari"));
+    userChoice = userChoice.toLowerCase();
+} while ( userChoice !== "pari" && userChoice !=="dispari");
+
 // scelta numero
 let numUser;
 do {
     numUser = Number(prompt("inserisci un numero tra 1 a 5"));
-} while ( isNaN(numUser) && num > 5);
+} while ( isNaN(numUser) && numUser > 5);
 
 const numCpu = getRandomNumber(1,5);
-console.log(numCpu)
+console.log(numCpu);
 const sum = numCpu + numUser;
 
 if (isOdd(sum)) {
-    console.log("il risultato è dispari : " + sum)
+    console.log("il risultato è dispari : " + sum);
 } else {
     console.log("il risultato è pari : " + sum)
+};
+
+if (userChoice === "dispari" && isOdd(sum)){
+    console.log("hai vinto")
+} else if ( userChoice === "pari" && !isOdd(sum)) {
+    console.log("hai vinto")
+} else {
+    console.log("hai perso")
 }
